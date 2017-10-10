5774c981d29bff84c15bdf0847cb62151e33e4f9
yusuf-farah <yfarah1577@gmail.com>
2017-10-10 11:52:51 -0400

    initial

---
+++ week3_project_classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_project_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boat boaty = new Boat(3, "Boaty", 18.6);
+            //boaty.CalculateMPG();
+        }   
+        // class_boat2= new class_boat(14, "Boat 2", 30.8);
+        //Boat2 Info()
+    }
+    }
+
---
+++ week3_project_classes/class boat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_project_classes
+{
+    class class_boat
+    {
+        class Boat
+        {
+            public int numberOfPassengers;// fields almost always set private
+
+    private string name;
+            private double enginesize;
+
+    public string Name
+                // expose private fields to other parts of our program//
+            {
+                get { return name; }
+                set { name = value; }//getting value from properties
+            }
+
+
+            public Boat()//default constructors use to create
+            {
+                // code to make the boat more
+            }
+
+
+            public Boat(int numberOfPassengers, string name, Double engineSize)
+            {
+            this.numberOfPassengers= numberOfPassengers;
+                this.name = name;
+                this.enginesize =   engineSize;
+
+            }
+
+
+            public void Move()//method is public so can access anywhere
+            {
+                //code to make the boat move
+               
+            }
+
+            private double CalculateMPG()//can access only inside class//
+            {
+                //code to calculate the MPG
+                double MPG = enginesize * .7d;
+                return MPG;
+            }
+                //    public void BoatInfo()
+                //Console.WriteLine("name:" +name);
+                //Console.WriteLine("passengers: " +numberOfPassengers);
+                //Console.WriteLine("Engine size: "+ engineSize);
+                //Console.WriteLine("mpg" +CalculatedMPG());
+               
+
+        }
+    }
+}
